d6268ed254eede56e20898b84583022a3100aad1
Cong Wang <cwang31@stanford.edu>
2023-12-26 18:02:02 -0800

    Correct the use of dataclasses in model configuration.

--- convnets/models/convnext/convnext.py
+++ convnets/models/convnext/convnext.py
@@ -20,8 +20,6 @@ import torch.nn.functional as F
 
 from dataclasses import asdict
 
-from .blocks import conv2d, pool2d
-
 from .convnext_config import DepthwiseSeparableConv2dConfig, Conv2dConfig, LayerNormConfig, ConvNeXTStemConfig, ConvNeXTBlockConfig, ConvNeXTStageConfig, ConvNeXTConfig
 
 
@@ -93,22 +91,13 @@ class ConvNeXTStem(nn.Module):
     def get_default_config():
         in_channels  = 1
         out_channels = 96
-        kernel_size  = 4
-        stride       = 4
-        conv_config = Conv2dConfig(
-            in_channels  = in_channels,
-            out_channels = out_channels,
-            kernel_size  = kernel_size,
-            stride       = stride,
-        )
+        H            = 256
+        W            = 256
 
-        C, H, W = 96, 256, 256
-        scale_factor = kernel_size
-        H //= scale_factor
-        W //= scale_factor
-        layer_norm_config = LayerNormConfig(normalized_shape = (C, H, W))
-
-        return ConvNeXTStemConfig(layer_norm = layer_norm_config, conv = conv_config)
+        return ConvNeXTStemConfig(in_channels  = in_channels,
+                                  out_channels = out_channels,
+                                  H            = H,
+                                  W            = W,)
 
 
     def __init__(self, config = None):
@@ -116,8 +105,8 @@ class ConvNeXTStem(nn.Module):
 
         self.config = ConvNeXTStem.get_default_config() if config is None else config
 
-        self.conv       = nn.Conv2d   (**asdict(self.config.conv      ))
-        self.layer_norm = nn.LayerNorm(**asdict(self.config.layer_norm))    # Normalize (1, C, H, W)
+        self.conv       = nn.Conv2d   (**asdict(self.config.conv_config))
+        self.layer_norm = nn.LayerNorm(**asdict(self.config.layer_norm_config))    # Normalize (1, C, H, W)
 
 
     def forward(self, x):
@@ -136,63 +125,22 @@ class ConvNeXTBlock(nn.Module):
     in_channels, mid_channels (bottleneck channels), out_channels
     """
 
-    @staticmethod
-    def get_default_config():
-        in_channels  = 96
-        mid_channels = 96 * 4
-        out_channels = 96
-
-        in_kernel_size  = 7
-        mid_kernel_size = 1
-        out_kernel_size = 1
-
-        in_padding  = (in_kernel_size  - 1) // 2
-        mid_padding = (mid_kernel_size - 1) // 2
-        out_padding = (out_kernel_size - 1) // 2
-
-        in_conv_config = DepthwiseSeparableConv2dConfig(in_channels  = in_channels,
-                                                        out_channels = in_channels,
-                                                        kernel_size  = in_kernel_size,
-                                                        padding      = in_padding,)    # ...Keep the spatial dimension unchanged
-
-        mid_conv_config = Conv2dConfig(in_channels  = in_channels,
-                                       out_channels = mid_channels,
-                                       kernel_size  = mid_kernel_size,
-                                       padding      = mid_padding,)
-
-        out_conv_config = Conv2dConfig(in_channels  = mid_channels,
-                                       out_channels = out_channels,
-                                       kernel_size  = out_kernel_size,
-                                       padding      = out_padding,)
-
-        C, H, W = 96, 256, 256
-        scale_factor = 4
-        H //= scale_factor
-        W //= scale_factor
-        layer_norm_config = LayerNormConfig(normalized_shape = (C, H, W))
-
-        return ConvNeXTBlockConfig(in_conv    = in_conv_config,
-                                   mid_conv   = mid_conv_config,
-                                   out_conv   = out_conv_config,
-                                   layer_norm = layer_norm_config)
-
-
-    def __init__(self, config = None):
+    def __init__(self, config):
         super().__init__()
 
-        self.config = ConvNeXTBlock.get_default_config() if config is None else config
+        self.config = config
 
         self.in_conv = nn.Sequential(
-            DepthwiseSeparableConv2d(**asdict(self.config.in_conv)),    # ...Keep the spatial dimension unchanged
-            nn.LayerNorm(**asdict(self.config.layer_norm))
+            DepthwiseSeparableConv2d(**asdict(self.config.in_conv_config)),    # ...Keep the spatial dimension unchanged
+            nn.LayerNorm(**asdict(self.config.layer_norm_config))
         )
 
         self.mid_conv = nn.Sequential(
-            nn.Conv2d(**asdict(self.config.mid_conv)),
+            nn.Conv2d(**asdict(self.config.mid_conv_config)),
             nn.GELU(),
         )
 
-        self.out_conv = nn.Conv2d(**asdict(self.config.out_conv))
+        self.out_conv = nn.Conv2d(**asdict(self.config.out_conv_config))
 
 
     def forward(self, x):
@@ -214,40 +162,15 @@ class ConvNeXTStage(nn.Module):
     Block means a res block.
     """
 
-    @staticmethod
-    def get_default_config():
-        return ConvNeXTStageConfig()
-
-    def __init__(self, stage_in_channels,
-                       stage_out_channels,
-                       num_blocks,
-                       mid_conv_channels,
-                       mid_conv_groups,
-                       in_conv_stride  = 1,
-                       mid_conv_stride = 1,
-                       config          = None):
+    def __init__(self, config):
         super().__init__()
 
-        self.config = ConvNeXTStage.get_default_config() if config is None else config
+        self.config = config
 
         # Process all blocks sequentially...
         self.blocks = nn.Sequential(*[
-            ConvNeXTBlock(
-                # First block uses stage_in_channels and rest uses prev stage_out_channels...
-                block_in_channels  = stage_in_channels if block_idx == 0 else stage_out_channels,
-
-                block_out_channels = stage_out_channels,
-                mid_conv_channels  = mid_conv_channels,
-                mid_conv_groups    = mid_conv_groups,
-
-                # First block uses in_conv_stride and rest uses 1...
-                in_conv_stride     = in_conv_stride    if block_idx == 0 else 1,
-                mid_conv_stride    = mid_conv_stride   if block_idx == 0 else 1,
-
-                # Other config...
-                config = self.config.RESBLOCK
-            )
-            for block_idx in range(num_blocks)
+            ConvNeXTBlock(config = block_config)
+            for block_config in self.config.block_config_list
         ])
 
 
@@ -280,96 +203,17 @@ class ConvNeXT(nn.Module):
 
         self.config = ConvNeXT.get_default_config() if config is None else config
 
-        # [[[ STEM ]]]
-        self.stem = ConvNeXTStem(stem_in_channels = 1, stem_out_channels = 64, config = self.config.RESSTEM)
+        self.stem = ConvNeXTStem(self.config.stem_config)
 
-        # [[[ Layer 1 ]]]
-        stage_in_channels  = 64
-        stage_out_channels = 256
-        mid_conv_channels  = stage_in_channels
-        num_stages         = 1
-        num_blocks         = 3
-        in_conv_stride     = 1
-        mid_conv_stride    = 1
-        self.layer1 = nn.Sequential(
-            pool2d(kernel_size = 3, stride = 2),    # ...Original ConvNeXTNet likes to have a pool in the first layer
-            *[ ConvNeXTStage(stage_in_channels  = stage_in_channels if stage_idx == 0 else stage_out_channels,
-                        stage_out_channels = stage_out_channels,
-                        num_blocks         = num_blocks,
-                        mid_conv_channels  = mid_conv_channels,
-                        mid_conv_groups    = 1,
-                        in_conv_stride     = in_conv_stride,
-                        mid_conv_stride    = mid_conv_stride,
-                        config             = self.config.RESSTAGE,)
-            for stage_idx in range(num_stages) ]
-        )
-
-        # [[[ Layer 2 ]]]
-        stage_in_channels  = 256
-        stage_out_channels = 512
-        mid_conv_channels  = 128
-        num_stages         = 1
-        num_blocks         = 4
-        in_conv_stride     = 1 if self.config.RESSTAGE.RESBLOCK.USES_RES_V1p5 else 2
-        mid_conv_stride    = 2 if self.config.RESSTAGE.RESBLOCK.USES_RES_V1p5 else 1
-        self.layer2 = nn.Sequential(*[
-            ConvNeXTStage(stage_in_channels  = stage_in_channels if stage_idx == 0 else stage_out_channels,
-                     stage_out_channels = stage_out_channels,
-                     num_blocks         = num_blocks,
-                     mid_conv_channels  = mid_conv_channels,
-                     mid_conv_groups    = 1,
-                     in_conv_stride     = in_conv_stride,
-                     mid_conv_stride    = mid_conv_stride,
-                     config             = self.config.RESSTAGE,)
-            for stage_idx in range(num_stages)
+        self.layers = nn.Sequential(*[
+            ConvNeXTStage(config = stage_config)
+            for stage_config in self.config.layers_config
         ])
 
-        # [[[ Layer 3 ]]]
-        stage_in_channels  = 512
-        stage_out_channels = 1024
-        mid_conv_channels  = 256
-        num_stages         = 1
-        num_blocks         = 6
-        in_conv_stride     = 1 if self.config.RESSTAGE.RESBLOCK.USES_RES_V1p5 else 2
-        mid_conv_stride    = 2 if self.config.RESSTAGE.RESBLOCK.USES_RES_V1p5 else 1
-        self.layer3 = nn.Sequential(*[
-            ConvNeXTStage(stage_in_channels  = stage_in_channels if stage_idx == 0 else stage_out_channels,
-                     stage_out_channels = stage_out_channels,
-                     num_blocks         = num_blocks,
-                     mid_conv_channels  = mid_conv_channels,
-                     mid_conv_groups    = 1,
-                     in_conv_stride     = in_conv_stride,
-                     mid_conv_stride    = mid_conv_stride,
-                     config             = self.config.RESSTAGE,)
-            for stage_idx in range(num_stages)
-        ])
-
-        # [[[ Layer 4 ]]]
-        stage_in_channels  = 1024
-        stage_out_channels = 2048
-        mid_conv_channels  = 512
-        num_stages         = 1
-        num_blocks         = 3
-        in_conv_stride     = 1 if self.config.RESSTAGE.RESBLOCK.USES_RES_V1p5 else 2
-        mid_conv_stride    = 2 if self.config.RESSTAGE.RESBLOCK.USES_RES_V1p5 else 1
-        self.layer4 = nn.Sequential(*[
-            ConvNeXTStage(stage_in_channels  = stage_in_channels if stage_idx == 0 else stage_out_channels,
-                     stage_out_channels = stage_out_channels,
-                     num_blocks         = num_blocks,
-                     mid_conv_channels  = mid_conv_channels,
-                     mid_conv_groups    = 1,
-                     in_conv_stride     = in_conv_stride,
-                     mid_conv_stride    = mid_conv_stride,
-                     config             = self.config.RESSTAGE,)
-            for stage_idx in range(num_stages)
-        ])
 
 
     def forward(self, x):
         x = self.stem(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
+        x = self.layers(x)
 
         return x
--- convnets/models/convnext/convnext_config.py
+++ convnets/models/convnext/convnext_config.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing      import Optional
+from typing      import Optional, List
 
 import torch
 
@@ -42,26 +42,162 @@ class LayerNormConfig:
 
 @dataclass
 class ConvNeXTStemConfig:
-    conv      : Conv2dConfig
-    layer_norm: LayerNormConfig
+    in_channels : int
+    out_channels: int
+    H           : int
+    W           : int
+
+    H_scaled    : int = field(init = False)
+    W_scaled    : int = field(init = False)
+
+    conv_config      : Conv2dConfig    = field(init = False)
+    layer_norm_config: LayerNormConfig = field(init = False)
+
+    def __post_init__(self):
+        in_channels  = self.in_channels
+        out_channels = self.out_channels
+        kernel_size  = 4
+        stride       = 4
+        self.conv_config = Conv2dConfig(
+            in_channels  = in_channels,
+            out_channels = out_channels,
+            kernel_size  = kernel_size,
+            stride       = stride,
+        )
+
+        scale_factor = 4
+        self.H_scaled = self.H // scale_factor
+        self.W_scaled = self.W // scale_factor
+        self.layer_norm_config = LayerNormConfig([out_channels, self.H_scaled, self.W_scaled])
+
+
 
 
 @dataclass
 class ConvNeXTBlockConfig:
-    in_conv   : DepthwiseSeparableConv2dConfig
-    layer_norm: LayerNormConfig
-    mid_conv  : Conv2dConfig
-    out_conv  : Conv2dConfig
+    # In conv
+    in_conv_in_channels: int
+    in_conv_stride     : int
 
-    uses_res_v1p5: bool = True
+    # Mid conv
+    mid_conv_out_channels: int
+    mid_conv_stride      : int
+
+    # Out conv
+    out_conv_out_channels: int
+
+    # Image dimension
+    H: int
+    W: int
+
+    in_conv_config   : DepthwiseSeparableConv2dConfig = field(init = False)
+    layer_norm_config: LayerNormConfig                = field(init = False)
+    mid_conv_config  : Conv2dConfig                   = field(init = False)
+    out_conv_config  : Conv2dConfig                   = field(init = False)
+
+    def __post_init__(self):
+        in_conv_kernel_size  = 7
+        mid_conv_kernel_size = 1
+        out_conv_kernel_size = 1
+
+        in_conv_padding  = (in_conv_kernel_size  - 1) // 2
+        mid_conv_padding = (mid_conv_kernel_size - 1) // 2
+        out_conv_padding = (out_conv_kernel_size - 1) // 2
+
+        self.in_conv_config = DepthwiseSeparableConv2dConfig(in_channels  = self.in_conv_in_channels,
+                                                             out_channels = self.in_conv_in_channels,
+                                                             kernel_size  = in_conv_kernel_size,
+                                                             padding      = in_conv_padding,)    # ...Keep the spatial dimension unchanged
+
+        self.mid_conv_config = Conv2dConfig(in_channels  = self.in_conv_in_channels,
+                                            out_channels = self.mid_conv_out_channels,
+                                            kernel_size  = mid_conv_kernel_size,
+                                            padding      = mid_conv_padding,)
+
+        self.out_conv_config = Conv2dConfig(in_channels  = self.mid_conv_out_channels,
+                                            out_channels = self.out_conv_out_channels,
+                                            kernel_size  = out_conv_kernel_size,
+                                            padding      = out_conv_padding,)
+
+        H = self.H
+        W = self.W
+        self.layer_norm_config = LayerNormConfig([self.in_conv_in_channels, H, W])
 
 
 @dataclass
 class ConvNeXTStageConfig:
-    RESBLOCK: ConvNeXTBlockConfig = field(default_factory = ConvNeXTBlockConfig)
+    stage_in_channels     : int
+    stage_out_channels    : int
+    num_blocks            : int
+    mid_conv_out_channels : int
+    in_conv_stride        : int
+    mid_conv_stride       : int
+
+    H: int
+    W: int
+
+    block_config_list: List[ConvNeXTBlockConfig] = field(init = False)
+
+    def __post_init__(self):
+        self.block_config_list = [
+            ConvNeXTBlockConfig(
+                # First block uses stage_in_channels and rest uses prev stage_out_channels...
+                in_conv_in_channels  = self.stage_in_channels if block_idx == 0 else self.stage_out_channels,
+
+                # First block uses in_conv_stride and rest uses 1...
+                in_conv_stride     = self.in_conv_stride    if block_idx == 0 else 1,
+                mid_conv_stride    = self.mid_conv_stride   if block_idx == 0 else 1,
+
+                mid_conv_out_channels = self.mid_conv_out_channels,
+                out_conv_out_channels = self.stage_out_channels,
+
+                H = self.H,
+                W = self.W,
+            )
+            for block_idx in range(self.num_blocks)
+        ]
+
 
 
 @dataclass
 class ConvNeXTConfig:
-    RESSTEM : ConvNeXTStemConfig  = field(default_factory = ConvNeXTStemConfig)
-    RESSTAGE: ConvNeXTStageConfig = field(default_factory = ConvNeXTStageConfig)
+    stem_in_channels : int = 1
+    stem_out_channels: int = 96
+
+    H: int = 256
+    W: int = 256
+
+    stage_in_channels_in_layer    : List[int] = field(default_factory = lambda: [96, 96, 96, 96])
+    stage_out_channels_in_layer   : List[int] = field(default_factory = lambda: [96, 96, 96, 96])
+    num_blocks_in_layer           : List[int] = field(default_factory = lambda: [3,  3,  9,  3 ])
+    mid_conv_out_channels_in_layer: List[int] = field(default_factory = lambda: [96, 96, 96, 96])
+    in_conv_stride_in_layer       : List[int] = field(default_factory = lambda: [1,  1,  1,  1 ])
+    mid_conv_stride_in_layer      : List[int] = field(default_factory = lambda: [1,  1,  1,  1 ])
+
+    stem_config  : ConvNeXTStemConfig = field(init = False)
+    layers_config: List[ConvNeXTStageConfig] = field(init = False)
+
+    def __post_init__(self):
+        self.stem_config = ConvNeXTStemConfig(
+            in_channels  = self.stem_in_channels,
+            out_channels = self.stem_out_channels,
+            H            = self.H,
+            W            = self.W,
+        )
+
+        H_scaled = self.stem_config.H_scaled
+        W_scaled = self.stem_config.W_scaled
+        num_layers = len(self.stage_in_channels_in_layer)
+        self.layers_config = [
+            ConvNeXTStageConfig(
+                stage_in_channels     = self.stage_in_channels_in_layer    [layer_idx],
+                stage_out_channels    = self.stage_out_channels_in_layer   [layer_idx],
+                num_blocks            = self.num_blocks_in_layer           [layer_idx],
+                mid_conv_out_channels = self.mid_conv_out_channels_in_layer[layer_idx],
+                in_conv_stride        = self.in_conv_stride_in_layer       [layer_idx],
+                mid_conv_stride       = self.mid_conv_stride_in_layer      [layer_idx],
+                H = H_scaled,
+                W = H_scaled,
+            )
+            for layer_idx in range(num_layers)
+        ]
