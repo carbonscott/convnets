59b6fa895c7d07d565c83e3c02464d66a1876b42
Cong Wang <cwang31@stanford.edu>
2023-12-28 22:11:54 -0800

    Document vanilla convnext with conv1x1 in the block.

---
+++ convnets/models/convnext/convnext_vanilla.py
@@ -0,0 +1,288 @@
+"""
+RegNet
+
+Building blocks
+- STEM
+- BODY
+  - STAGES (1..4)
+    - BLOCKS (1..N)
+- HEAD
+
+width_per_stage: specifies the width (i.e., number of channels) for each stage.
+"""
+
+import torch
+import torch.nn            as nn
+import torch.nn.functional as F
+
+from dataclasses import asdict
+
+from .convnext_config import DepthwiseSeparableConv2dConfig, Conv2dConfig, LayerNormConfig, ConvNeXTStemConfig, ConvNeXTBlockConfig, ConvNeXTStageConfig, ConvNeXTConfig
+
+
+class DepthwiseSeparableConv2d(nn.Module):
+    """
+    As the name suggests, it's a conv2d done in two steps:
+    - Spatial only conv, no inter-channel communication.
+    - Inter-channel communication, no spatial communication.
+    """
+
+    def __init__(self, in_channels,
+                       out_channels,
+                       kernel_size  = 1,
+                       stride       = 1,
+                       padding      = 0,
+                       dilation     = 1,
+                       bias         = True,
+                       padding_mode = 'zeros',
+                       device       = None,
+                       dtype        = None):
+        super().__init__()
+
+        # Depthwise conv means channels are independent, only spatial bits communicate
+        # Essentially it simply scales every tensor element
+        self.depthwise_conv = nn.Conv2d(in_channels  = in_channels,
+                                        out_channels = in_channels,
+                                        kernel_size  = kernel_size,
+                                        stride       = stride,
+                                        padding      = padding,
+                                        dilation     = dilation,
+                                        groups       = in_channels,    # Input channels don't talk to each other
+                                        bias         = bias,
+                                        padding_mode = padding_mode,
+                                        device       = device,
+                                        dtype        = dtype)
+
+        # Pointwise to facilitate inter-channel communication, no spatial bits communicate
+        self.pointwise_conv = nn.Conv2d(in_channels  = in_channels,
+                                        out_channels = out_channels,
+                                        kernel_size  = 1,
+                                        stride       = 1,
+                                        padding      = 0,
+                                        dilation     = 1,
+                                        groups       = 1,    # Input channels don't talk to each other
+                                        bias         = bias,
+                                        padding_mode = padding_mode,
+                                        device       = device,
+                                        dtype        = dtype)
+
+
+    def forward(self, x):
+        x = self.depthwise_conv(x)
+        x = self.pointwise_conv(x)
+
+        return x
+
+
+class DepthwiseConv2d(nn.Module):
+    """
+    As the name suggests, it's a conv2d done in two steps:
+    - Spatial only conv, no inter-channel communication.
+    """
+
+    def __init__(self, in_channels,
+                       out_channels,
+                       kernel_size  = 1,
+                       stride       = 1,
+                       padding      = 0,
+                       dilation     = 1,
+                       bias         = True,
+                       padding_mode = 'zeros',
+                       device       = None,
+                       dtype        = None):
+        super().__init__()
+
+        # Depthwise conv means channels are independent, only spatial bits communicate
+        # Essentially it simply scales every tensor element
+        self.depthwise_conv = nn.Conv2d(in_channels  = in_channels,
+                                        out_channels = in_channels,
+                                        kernel_size  = kernel_size,
+                                        stride       = stride,
+                                        padding      = padding,
+                                        dilation     = dilation,
+                                        groups       = in_channels,    # Input channels don't talk to each other
+                                        bias         = bias,
+                                        padding_mode = padding_mode,
+                                        device       = device,
+                                        dtype        = dtype)
+
+
+    def forward(self, x):
+        x = self.depthwise_conv(x)
+
+        return x
+
+
+class ChannelwiseLayerNorm(nn.Module):
+    def __init__(self, layer_norm_config):
+        super().__init__()
+
+        self.layer_norm = nn.LayerNorm(**asdict(layer_norm_config))
+
+
+    def forward(self, x):
+        return self.layer_norm(x.permute(0, 2, 3, 1)).permute(0, 3, 1, 2)    # (B, C, H, W) -> (B, H, W, C) -> (B, C, H, W)
+
+
+
+class ConvNeXTStem(nn.Module):
+    """
+    This class implments the first layer (STEM in RegNet's nomenclature) of
+    ConvNeXT.
+
+    Spatial dimension change: (1, H, W) -> (C, H//4, W//4)
+
+    Each patch is turned into a C-dimension vector.
+    """
+
+    @staticmethod
+    def get_default_config():
+        in_channels  = 1
+        out_channels = 96
+
+        return ConvNeXTStemConfig(in_channels  = in_channels,
+                                  out_channels = out_channels,)
+
+
+    def __init__(self, config = None):
+        super().__init__()
+
+        self.config = ConvNeXTStem.get_default_config() if config is None else config
+
+        self.conv       = nn.Conv2d   (**asdict(self.config.conv_config))
+        self.layer_norm = ChannelwiseLayerNorm(self.config.layer_norm_config)    # Normalize (1, C, H, W)
+
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.layer_norm(x)
+
+        return x
+
+
+
+
+class ConvNeXTBlock(nn.Module):
+    """
+    Create X blocks for the RegNet architecture.
+
+    in_channels, mid_channels (bottleneck channels), out_channels
+    """
+
+    def __init__(self, config):
+        super().__init__()
+
+        self.config = config
+
+        self.in_conv = nn.Sequential(
+            DepthwiseConv2d(**asdict(self.config.in_conv_config)),    # ...Keep the spatial dimension unchanged
+            ChannelwiseLayerNorm(self.config.layer_norm_config),
+        )
+
+        self.mid_conv = nn.Sequential(
+            nn.Conv2d(**asdict(self.config.mid_conv_config)),
+            nn.GELU(),
+        )
+
+        self.out_conv = nn.Conv2d(**asdict(self.config.out_conv_config))
+
+
+    def forward(self, x):
+        y = self.in_conv(x)
+        y = self.mid_conv(y)
+        y = self.out_conv(y)
+
+        y = y + x
+
+        return y
+
+
+
+
+class ConvNeXTStage(nn.Module):
+    """
+    This class implments the one stage in the RegNet architecture.
+
+    Block means a res block.
+    """
+
+    def __init__(self, config):
+        super().__init__()
+
+        self.config = config
+
+        # Process all blocks sequentially...
+        self.blocks = nn.Sequential(*[
+            ConvNeXTBlock(config = block_config)
+            for block_config in self.config.block_config_list
+        ])
+
+
+    def forward(self, x):
+        x = self.blocks(x)
+
+        return x
+
+
+
+
+class ConvNeXTStageJoint(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+
+        self.config = config
+
+        self.stage_joint = nn.Sequential(
+            ChannelwiseLayerNorm(self.config.layer_norm_config),
+            nn.Conv2d(**asdict(self.config.conv_config)),
+        )
+
+
+    def forward(self, x):
+        return self.stage_joint(x)
+
+
+
+
+class ConvNeXT(nn.Module):
+    """
+    This class implements single channel ConvNeXT using the RegNet
+    nomenclature.
+
+    ConvNeXT architecture reference: [NEED URL]
+
+    ConvNeXTStage(s) are kept in nn.Sequential but not nn.ModuleList since they will
+    be processed sequentially.
+    """
+
+    @staticmethod
+    def get_default_config():
+        return ConvNeXTConfig()
+
+
+    def __init__(self, config = None):
+        super().__init__()
+
+        self.config = ConvNeXT.get_default_config() if config is None else config
+
+        self.stem = ConvNeXTStem(self.config.stem_config)
+
+        self.stages = nn.ModuleList([
+            ConvNeXTStage(config = stage_config)
+            for stage_config in self.config.stages_config
+        ])
+
+        self.stage_joints = nn.ModuleList([
+            nn.Identity() if stage_joint_config is None else
+            ConvNeXTStageJoint(config = stage_joint_config)
+            for stage_joint_config in self.config.stage_joints_config
+        ])
+
+
+    def forward(self, x):
+        x = self.stem(x)
+        for stage_joint, stage in zip(self.stage_joints, self.stages):
+            x = stage_joint(x)
+            x = stage(x)
+
+        return x
---
+++ convnets/models/convnext/convnext_vanilla_config.py
@@ -0,0 +1,226 @@
+from dataclasses import dataclass, field
+from typing      import Optional, List
+
+import torch
+import torch.nn as nn
+
+
+@dataclass
+class DepthwiseSeparableConv2dConfig:
+    in_channels : int
+    out_channels: int
+    kernel_size : int
+    stride      : int = 1
+    padding     : int = 0
+    dilation    : int = 1
+    bias        : int = True
+    padding_mode: int = 'zeros'
+    device      : Optional[torch.device] = None
+    dtype       : Optional[torch.dtype]  = None
+
+
+@dataclass
+class DepthwiseConv2dConfig:
+    in_channels : int
+    out_channels: int
+    kernel_size : int
+    stride      : int = 1
+    padding     : int = 0
+    dilation    : int = 1
+    bias        : int = True
+    padding_mode: int = 'zeros'
+    device      : Optional[torch.device] = None
+    dtype       : Optional[torch.dtype]  = None
+
+
+@dataclass
+class Conv2dConfig:
+    in_channels : int
+    out_channels: int
+    kernel_size : int
+    stride      : int  = 1
+    padding     : int  = 0
+    dilation    : int  = 1
+    groups      : int  = 1
+    bias        : bool = True
+    padding_mode: str  = 'zeros'
+    device      : Optional[torch.device] = None
+    dtype       : Optional[torch.dtype]  = None
+
+
+@dataclass
+class LayerNormConfig:
+    normalized_shape  : int
+    eps               : float = 1e-6
+    elementwise_affine: bool  = True
+
+
+@dataclass
+class ConvNeXTStemConfig:
+    in_channels : int
+    out_channels: int
+
+    conv_config: Conv2dConfig = field(init = False)
+
+    def __post_init__(self):
+        in_channels  = self.in_channels
+        out_channels = self.out_channels
+        kernel_size  = 4
+        stride       = 4
+        self.conv_config = Conv2dConfig(
+            in_channels  = in_channels,
+            out_channels = out_channels,
+            kernel_size  = kernel_size,
+            stride       = stride,
+        )
+
+        self.layer_norm_config = LayerNormConfig(normalized_shape = out_channels)
+
+
+@dataclass
+class ConvNeXTBlockConfig:
+    # In conv
+    in_conv_in_channels: int
+    in_conv_stride     : int
+
+    # Mid conv
+    mid_conv_out_channels: int
+    mid_conv_stride      : int
+
+    # Out conv
+    out_conv_out_channels: int
+
+    in_conv_config   : DepthwiseConv2dConfig = field(init = False)
+    layer_norm_config: LayerNormConfig       = field(init = False)
+    mid_conv_config  : Conv2dConfig          = field(init = False)
+    out_conv_config  : Conv2dConfig          = field(init = False)
+
+    def __post_init__(self):
+        in_conv_in_channels   = self.in_conv_in_channels
+        mid_conv_out_channels = self.mid_conv_out_channels
+        out_conv_out_channels = self.out_conv_out_channels
+
+        in_conv_kernel_size  = 7
+        mid_conv_kernel_size = 1
+        out_conv_kernel_size = 1
+
+        in_conv_padding  = (in_conv_kernel_size  - 1) // 2
+        mid_conv_padding = (mid_conv_kernel_size - 1) // 2
+        out_conv_padding = (out_conv_kernel_size - 1) // 2
+
+        self.in_conv_config = DepthwiseConv2dConfig(in_channels  = in_conv_in_channels,
+                                                    out_channels = in_conv_in_channels,
+                                                    kernel_size  = in_conv_kernel_size,
+                                                    padding      = in_conv_padding,)    # ...Keep the spatial dimension unchanged
+
+        self.mid_conv_config = Conv2dConfig(in_channels  = in_conv_in_channels,
+                                            out_channels = mid_conv_out_channels,
+                                            kernel_size  = mid_conv_kernel_size,
+                                            padding      = mid_conv_padding,)
+
+        self.out_conv_config = Conv2dConfig(in_channels  = mid_conv_out_channels,
+                                            out_channels = out_conv_out_channels,
+                                            kernel_size  = out_conv_kernel_size,
+                                            padding      = out_conv_padding,)
+
+        self.layer_norm_config = LayerNormConfig(normalized_shape = in_conv_in_channels)
+
+
+@dataclass
+class ConvNeXTStageConfig:
+    stage_in_channels     : int
+    stage_out_channels    : int
+    num_blocks            : int
+    mid_conv_out_channels : int
+    in_conv_stride        : int
+    mid_conv_stride       : int
+
+    block_config_list: List[ConvNeXTBlockConfig] = field(init = False)
+
+    def __post_init__(self):
+        stage_in_channels     = self.stage_in_channels
+        stage_out_channels    = self.stage_out_channels
+        num_blocks            = self.num_blocks
+        mid_conv_out_channels = self.mid_conv_out_channels
+        in_conv_stride        = self.in_conv_stride
+        mid_conv_stride       = self.mid_conv_stride
+        self.block_config_list = [
+            ConvNeXTBlockConfig(
+                # First block uses stage_in_channels and rest uses prev stage_out_channels...
+                in_conv_in_channels = stage_in_channels if block_idx == 0 else stage_out_channels,
+
+                # First block uses in_conv_stride and rest uses 1...
+                in_conv_stride  = in_conv_stride  if block_idx == 0 else 1,
+                mid_conv_stride = mid_conv_stride if block_idx == 0 else 1,
+
+                mid_conv_out_channels = mid_conv_out_channels,
+                out_conv_out_channels = stage_out_channels,
+            )
+            for block_idx in range(num_blocks)
+        ]
+
+
+@dataclass
+class ConvNextStageJointConfig:
+    in_channels : int
+    out_channels: int
+
+    conv_config      : Conv2dConfig    = field(init = False)
+    layer_norm_config: LayerNormConfig = field(init = False)
+
+    def __post_init__(self):
+        in_channels  = self.in_channels
+        out_channels = self.out_channels
+        kernel_size  = 2
+        stride       = 2
+        self.conv_config = Conv2dConfig(in_channels  = in_channels,
+                                        out_channels = out_channels,
+                                        kernel_size  = kernel_size,
+                                        stride       = stride,)
+
+        self.layer_norm_config = LayerNormConfig(normalized_shape = out_channels)
+
+
+@dataclass
+class ConvNeXTConfig:
+    stem_in_channels : int = 1
+    stem_out_channels: int = 96
+
+    stage_in_channels_list    : List[int] = field(default_factory = lambda: [96    , 96 * 2, 96 * 4 , 96 * 8 ])
+    mid_conv_out_channels_list: List[int] = field(default_factory = lambda: [96 * 4, 96 * 8, 96 * 16, 96 * 32])
+    stage_out_channels_list   : List[int] = field(default_factory = lambda: [96    , 96 * 2, 96 * 4 , 96 * 8 ])
+    num_blocks_list           : List[int] = field(default_factory = lambda: [3,  3,  9,  3 ])
+    in_conv_stride_list       : List[int] = field(default_factory = lambda: [1,  1,  1,  1 ])
+    mid_conv_stride_list      : List[int] = field(default_factory = lambda: [1,  1,  1,  1 ])
+
+    stem_config        : ConvNeXTStemConfig             = field(init = False)
+    stages_config      : List[ConvNeXTStageConfig]      = field(init = False)
+    stage_joints_config: List[ConvNextStageJointConfig] = field(init = False)
+
+    def __post_init__(self):
+        self.stem_config = ConvNeXTStemConfig(
+            in_channels  = self.stem_in_channels,
+            out_channels = self.stem_out_channels,
+        )
+
+        num_stages = len(self.stage_in_channels_list)
+        self.stages_config = [
+            ConvNeXTStageConfig(
+                stage_in_channels     = self.stage_in_channels_list    [stage_idx],
+                stage_out_channels    = self.stage_out_channels_list   [stage_idx],
+                num_blocks            = self.num_blocks_list           [stage_idx],
+                mid_conv_out_channels = self.mid_conv_out_channels_list[stage_idx],
+                in_conv_stride        = self.in_conv_stride_list       [stage_idx],
+                mid_conv_stride       = self.mid_conv_stride_list      [stage_idx],
+            )
+            for stage_idx in range(num_stages)
+        ]
+
+        self.stage_joints_config = [
+            None if stage_idx == 0 else
+            ConvNextStageJointConfig(
+                in_channels  = self.stage_out_channels_list[stage_idx - 1],    # Out channel of the stage before the joint
+                out_channels = self.stage_in_channels_list [stage_idx],        # In channel of the stage after the joint
+            )
+            for stage_idx in range(num_stages)
+        ]
